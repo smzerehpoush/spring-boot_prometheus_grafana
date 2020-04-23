f46fe2a849dede9ab7ef8c3544b6dce9f81b29c1
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-04-23 14:14:51 +0430

    change structure of project

---
+++ spring-boot-v1.5/src/main/java/mahdiyar/controller/MyController.java
@@ -0,0 +1,20 @@
+package mahdiyar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Seyyed Mahdiyar Zerehpoush
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class MyController {
+
+    @GetMapping("/test")
+    public @ResponseBody
+    String test() {
+        return "OK";
+    }
+}
